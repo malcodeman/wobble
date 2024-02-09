cf87f062885f948412e4f05c4cfe8dbecc799e02
malcodeman <26983729+malcodeman@users.noreply.github.com>
2024-02-09 11:49:36 +0100

    fix: adds button component ref

--- src/app/ui/Button.tsx
+++ src/app/ui/Button.tsx
@@ -1,15 +1,16 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes, forwardRef } from "react";
 
 type Props = {
   icon?: React.ReactNode;
   children?: React.ReactNode;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button = (props: Props) => {
+export const Button = forwardRef<HTMLButtonElement, Props>((props, ref) => {
   const { icon, children, ...rest } = props;
 
   return (
     <button
+      ref={ref}
       type="button"
       className="inline-flex items-center gap-x-1.5 rounded-md bg-white/10 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-white/20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
       {...rest}
@@ -18,4 +19,6 @@ export const Button = (props: Props) => {
       {children}
     </button>
   );
-};
+});
+
+Button.displayName = "Button";
