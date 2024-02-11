09ddac7dc68056ac171526ca3fe15535a044cc52
malcodeman <26983729+malcodeman@users.noreply.github.com>
2024-02-11 15:01:04 +0100

    feat: adds project header

--- src/app/projects/[id]/components/Canvas.tsx
+++ src/app/projects/[id]/components/Canvas.tsx
@@ -4,12 +4,14 @@ import { useMeasure, useTimeoutEffect } from "@react-hookz/web";
 import { useEffect, useState } from "react";
 import randomcolor from "randomcolor";
 import {
+  IconHome,
   IconPlayerPauseFilled,
   IconPlayerPlayFilled,
   IconRepeat,
 } from "@tabler/icons-react";
 import { saveAs } from "file-saver";
 import { useParams } from "next/navigation";
+import Link from "next/link";
 import { Button } from "@/app/ui/Button";
 import { DrawableShape, ShapeType } from "@/app/types";
 import { Divider } from "@/app/ui/Divider";
@@ -108,82 +110,93 @@ const Canvas = () => {
   };
 
   return (
-    <div ref={ref} className="grid h-screen grid-cols-[1fr_240px]">
-      {measurements ? (
-        <Stage
-          width={measurements.width - 240}
-          height={measurements.height}
-          options={{ backgroundColor: 0xfcfcfd }}
-        >
-          {shapes.map((shape, index) => (
-            <Shape
-              key={index}
-              draw={(g) => onDraw(g, shape)}
-              onColorChange={() => handleOnColorChange(index)}
-              isPlaying={play}
-              x={shape.x}
-              y={shape.y}
-              duration={Number(duration)}
+    <div ref={ref} className="h-[calc(100vh-60px)]">
+      <header className="p-4">
+        <div className="flex items-center gap-2">
+          <Link href="/">
+            <Button icon={<IconHome size={16} />} />
+          </Link>
+          <span className="text-white">{project?.title}</span>
+        </div>
+      </header>
+      <div className="grid grid-cols-[1fr_240px]">
+        {measurements ? (
+          <Stage
+            width={measurements.width - 240}
+            height={measurements.height}
+            options={{ backgroundColor: 0xfcfcfd }}
+            className="rounded-tr-md"
+          >
+            {shapes.map((shape, index) => (
+              <Shape
+                key={index}
+                draw={(g) => onDraw(g, shape)}
+                onColorChange={() => handleOnColorChange(index)}
+                isPlaying={play}
+                x={shape.x}
+                y={shape.y}
+                duration={Number(duration)}
+              />
+            ))}
+          </Stage>
+        ) : null}
+        <div className="px-2">
+          <div className="flex flex-col gap-2">
+            <span className="isolate inline-flex">
+              <Button
+                onClick={() => setActiveShape("rectangle")}
+                isActive={activeShape === "rectangle"}
+                className="rounded-none rounded-l-md"
+              >
+                Rectangle
+              </Button>
+              <Button
+                onClick={() => setActiveShape("ellipse")}
+                isActive={activeShape === "ellipse"}
+                className="-ml-px rounded-none"
+              >
+                Ellipse
+              </Button>
+              <Button
+                onClick={() => setActiveShape("polygon")}
+                isActive={activeShape === "polygon"}
+                className="-ml-px rounded-none rounded-r-md"
+              >
+                Polygon
+              </Button>
+            </span>
+            <Button onClick={handleOnAdd}>Add shape</Button>
+            <Divider className="my-4" />
+            <Input
+              label="Duration"
+              rightSection="sec"
+              value={duration}
+              onChange={(e) => setDuration(e.currentTarget.value)}
             />
-          ))}
-        </Stage>
-      ) : null}
-      <div>
-        <div className="flex flex-col gap-2 p-2">
-          <span className="isolate inline-flex">
-            <Button
-              onClick={() => setActiveShape("rectangle")}
-              isActive={activeShape === "rectangle"}
-              className="rounded-none rounded-l-md"
-            >
-              Rectangle
-            </Button>
-            <Button
-              onClick={() => setActiveShape("ellipse")}
-              isActive={activeShape === "ellipse"}
-              className="-ml-px rounded-none"
-            >
-              Ellipse
-            </Button>
-            <Button
-              onClick={() => setActiveShape("polygon")}
-              isActive={activeShape === "polygon"}
-              className="-ml-px rounded-none rounded-r-md"
-            >
-              Polygon
-            </Button>
-          </span>
-          <Button onClick={handleOnAdd}>Add shape</Button>
-          <Divider className="my-4" />
-          <Input
-            label="Duration"
-            rightSection="sec"
-            value={duration}
-            onChange={(e) => setDuration(e.currentTarget.value)}
-          />
-          <div className="inline-flex gap-2">
-            <Button
-              className="w-full"
-              icon={
-                play ? (
-                  <IconPlayerPauseFilled size={16} />
-                ) : (
-                  <IconPlayerPlayFilled size={16} />
-                )
-              }
-              onClick={() => setPlay(!play)}
-            >
-              {play ? "Pause" : "Play"}
-            </Button>
-            <Button
-              isActive={repeat}
-              icon={<IconRepeat size={16} />}
-              onClick={handleOnRepeat}
-            />
+            <div className="inline-flex gap-2">
+              <Button
+                className="w-full"
+                icon={
+                  play ? (
+                    <IconPlayerPauseFilled size={16} />
+                  ) : (
+                    <IconPlayerPlayFilled size={16} />
+                  )
+                }
+                onClick={() => setPlay(!play)}
+              >
+                {play ? "Pause" : "Play"}
+              </Button>
+              <Button
+                isActive={repeat}
+                icon={<IconRepeat size={16} />}
+                onClick={handleOnRepeat}
+              />
+            </div>
+            <Divider className="my-4" />
+            <FileUpload onDrop={handleOnImport} />
+            <Button onClick={handleOnDownload}>Download .json</Button>
           </div>
-          <Divider className="my-4" />
-          <FileUpload onDrop={handleOnImport} />
-          <Button onClick={handleOnDownload}>Download .json</Button>
         </div>
       </div>
     </div>
